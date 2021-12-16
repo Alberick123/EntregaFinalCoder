a6011ac18d09fbe8a58829b910c13ed4a6b2e404
Alberick123 <95361704+Alberick123@users.noreply.github.com>
2021-12-15 22:44:40 -0300

    Se agregaron las clases en admin

--- ProyectoClub/AppClub/admin.py
+++ ProyectoClub/AppClub/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(Jugador)
+
+admin.site.register(Disciplina)
+
+admin.site.register(Estadio)
