d2e3b6b5d6f90a843f0c8f93c1b915faa9e0fcbb
Alan1590 <awg.1590@gmail.com>
2021-12-16 16:01:29 -0300

    Se agregan form faltantes, se actualizan templates

--- ProyectoClub/AppClub/forms.py
+++ ProyectoClub/AppClub/forms.py
@@ -5,3 +5,17 @@ class EstadioForm(forms.Form):
     nombre = forms.CharField(max_length=40)
     direccion = forms.CharField(max_length=40)
     anioFund = forms.DateField(initial=datetime.date.today)
+
+
+class DisciplinaForm(forms.Form):
+    
+    nombre = forms.CharField(max_length=40)
+    sede = forms.CharField(max_length=40)
+    dias_realizacion = forms.DateField()
+
+
+class JugadorForm(forms.Form):
+    
+    nombre = forms.CharField(max_length=40)
+    altura = forms.FloatField()
+    edad = forms.IntegerField()
